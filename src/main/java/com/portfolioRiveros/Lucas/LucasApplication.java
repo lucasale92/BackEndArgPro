d7ab3ed968a4840154028d98c9ee40a9a4504ccd
lucasale92 <riveros.lucas07@gmail.com>
2022-10-29 23:18:15 -0300

    correccion

--- src/main/java/com/portfolioRiveros/Lucas/LucasApplication.java
+++ src/main/java/com/portfolioRiveros/Lucas/LucasApplication.java
@@ -2,12 +2,31 @@ package com.portfolioRiveros.Lucas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LucasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LucasApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LucasApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+
+    }
 
 }
+
+

824daf7fbe7a7d4fcd5b4cd37c30abb4e4c5c58c
lucasale92 <riveros.lucas07@gmail.com>
2022-10-31 14:37:06 -0300

    mainsecurity

--- src/main/java/com/portfolioRiveros/Lucas/Security/MainSecurity.java
+++ src/main/java/com/portfolioRiveros/Lucas/Security/MainSecurity.java
@@ -49,7 +49,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/auth/login", "/auth/nuevo").permitAll()
+                .antMatchers("**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
